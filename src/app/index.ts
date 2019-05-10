74d8d95f1f09d431522bb85b12d7a514fdc31904
James <jamesdawsonwd@gmail.com>
2019-05-10 10:36:28 +0100

    remove: wasted import

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,8 @@
-// tslint:disable-next-line:no-import-side-effect
-import '../public/js/canvas';
-// tslint:disable-next-line:no-import-side-effect
 import '../sass/app.scss';
 import {Canvas} from '../public/js/canvas';
 
 function init() {
-    const canvas = document.querySelector("canvas");
+    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
     new Canvas(canvas);
 }
 
